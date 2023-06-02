d18e14e937a14f2de3d3f4d2d5d25ee3dc3432a2
manisha guda <manishavguda@gmail.com>
2023-06-02 15:17:55 +0530

    jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,8 +2,26 @@ def compile() {
     if (app_lang == "nodejs") {
         sh 'npm install'
     }
+
     if (app_lang == "maven") {
         sh 'mvn package'
     }
 
+}
+
+def unittest() {
+
+    if (app_lang == "nodejs") {
+        //Developer is missing unit test cases in projects, He need to add them as best practice, We are skipping to proceed forthure
+        sh 'npm test || true'
+
+    }
+
+    if (app_lang == "maven") {
+        sh 'mvn test'
+    }
+
+    if (app_lang == "python") {
+        sh 'python3 -m unittest'
+    }
 }
