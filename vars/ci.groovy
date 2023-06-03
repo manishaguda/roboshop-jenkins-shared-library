5b0882f5f45536f4db78583caf596ec6cb84cb77
manisha guda <manishavguda@gmail.com>
2023-06-03 18:00:39 +0530

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -25,16 +25,16 @@ def call() {
                     }
                 }
                 stage('Quality Control') {
-                  environment {
-                              SONAR_USER = $'(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption  --query Parameters[0].Value | sed 's/"//g\')'
-                              SONAR_PASS = $'(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption  --query Parameters[0].Value | sed 's/"//g\')'
-                   }
-
+                 steps {
+                     withAWSParameterStore(credentialsId: '', naming: 'relative', path: '/service', recursive: true, regionName: 'eu-west-1') {
+                         {
+                             echo "USER = ${env.SONARQUBE_USER}"
+                     }
 
                     }
-                    steps {
 
-                        sh 'sonar-scanner -Dsonar.host.url=http://172.31.81.169:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
+
+                        //'sonar-scanner -Dsonar.host.url=http://172.31.81.169:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
 
                     }
                 }
