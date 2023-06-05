3d0c0df308b35dfca489478e3b2549e6ec48b831
manisha guda <manishavguda@gmail.com>
2023-06-05 17:36:30 +0530

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,7 +2,7 @@ def call() {
     try {
         node('workstation') {
 
-            stage('Checkout') {
+            //stage('Checkout') {
                 cleanWs()
                 git branch: 'main', url: "https://github.com/manishaguda/${COMPONENT}"
             }
