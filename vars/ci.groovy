4dfb8bf0ef7ff3c690bbe3f66baa83817838b19e
manisha guda <manishavguda@gmail.com>
2023-06-07 09:39:15 +0530

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -18,32 +18,32 @@ def call() {
             }
 
             stage('Quality Control') {
-                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                     sh "sonar-scanner -Dsonar.host.url=http://172.31.81.169:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${COMPONENT}"
                 }
 
             }
 
-        if (app_lang == "maven") {
-            stage('Build Package') {
-                sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
+            if (app_lang == "maven") {
+                stage('Build Package') {
+                    sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
+                }
             }
-        }
 
 
             stage('Upload Code To Centralized Place') {
                 echo 'upload'
             }
 
-          }
+        }
 
-          } catch(Exception e) {
+    } catch (Exception e) {
         common.email("Failed")
     }
 
-
+}
 
 
 
