9605c45b648656b711a3167d417804980d3743da
manisha guda <manishavguda@gmail.com>
2023-06-09 17:28:52 +0530

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,6 +4,12 @@ def call() {
         env.SONAR_EXTRA_OPTS = " "
     }
 
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "false"
+    } else {
+        env.PUSH_CODE = "true"
+    }
+
     try {
         node('workstation') {
 
@@ -39,10 +45,12 @@ def call() {
                 }
             }
 
-
-            stage('Upload Code To Centralized Place') {
-                echo 'upload'
+            if(env.PUSH_CODE = "true"){
+                stage('Upload Code To Centralized Place') {
+                    echo 'upload'
+                }
             }
+
 
         }
 
