2964a0a867d59ba4745376aa72f6f843c221cc1b
manisha guda <manishavguda@gmail.com>
2023-06-13 12:11:20 +0530

    /

--- vars/common.groovy
+++ vars/common.groovy
@@ -31,7 +31,7 @@ def email(email_note) {
 }
 
 def artifactPush() {
-    sh "echo ${TAG_NAME} <VERSION"
+    sh "echo ${TAG_NAME} >VERSION"
     if (app_lang == "nodejs") {
      sh  "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
 
